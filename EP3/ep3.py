ac1adde67638b6573c427f8dc5b4bc7dcfd53f6a
Isabela Blucher <isabelablucher@gmail.com>
2018-11-25 21:50:49 -0200

    EP3 finalizado

--- EP3/ep3.py
+++ EP3/ep3.py
@@ -1,8 +1,32 @@
+  # AO PREENCHER ESSE CABEÇALHO COM O MEU NOME E O MEU NÚMERO USP,
+  # DECLARO QUE SOU O ÚNICO AUTOR E RESPONSÁVEL POR ESSE PROGRAMA.
+  # TODAS AS PARTES ORIGINAIS DESSE EXERCÍCIO-PROGRAMA (EP) FORAM
+  # DESENVOLVIDAS E IMPLEMENTADAS POR MIM SEGUINDO AS INSTRUÇÕES
+  # DESSE EP E QUE PORTANTO NÃO CONSTITUEM DESONESTIDADE ACADÊMICA
+  # OU PLÁGIO.
+  # DECLARO TAMBÉM QUE SOU RESPONSÁVEL POR TODAS AS CÓPIAS DESSE
+  # PROGRAMA E QUE EU NÃO DISTRIBUI OU FACILITEI A SUA DISTRIBUIÇÃO.
+  # ESTOU CIENTE QUE OS CASOS DE PLÁGIO E DESONESTIDADE ACADÊMICA
+  # SERÃO SEVERAMENTE PUNIDOS.
+  # ENTENDO QUE EPS SEM DECLARAÇÃO DE RESPONSABILIDADE NÃO SERÃO
+  # CORRIGIDOS E PODERÃO CAUSAR PUNIÇÃO DO AUTOR POR DESONESTIDADE
+  # ACADÊMICA.
+  #
+  # Nome: Isabela Blucher
+  # Número USP: 9298170
+  # Curso: Biologia Computacional - MAC0465         Prof. Alair
+  # Exercício-Programa 3
+  #
+  #  OBS: o algoritmo de ordenação topológica tem como fonte: https://www.geeksforgeeks.org/topological-sorting/
+
+# -*- coding: utf-8 -*-
+
 import sys
 import unittest
 import numpy as np
 from collections import defaultdict
 
+# devolve o overlap entre dois fragmentos
 def getOverlap(left, right):
     overlap = ""
     for c in right:
@@ -15,6 +39,7 @@ def getOverlap(left, right):
     return
 
 
+# constrói o grafo de overlap dos fragmentos
 def buildGraph(F, t):
     graph = defaultdict(list)
     weights = {}
@@ -32,6 +57,7 @@ def buildGraph(F, t):
     return graph, weights, overlaps
 
 
+# função recursiva para a ordenação topológica
 def topologicalSortRec(graph, v, visited, stack):
     visited[v] = True
 
@@ -42,6 +68,7 @@ def topologicalSortRec(graph, v, visited, stack):
     stack.insert(0, v)
 
 
+# chamada principal para a ordenação topológica
 def topologialSort(graph, V):
     visited = [False]*V
     stack = []
@@ -50,9 +77,43 @@ def topologialSort(graph, V):
         if not visited[i]:
             topologicalSortRec(graph, i, visited, stack)
 
-    print(stack)
+    return stack
 
 
+# imprime os vértices do grafo
+def printVertex(F):
+    print('Vértices:')
+    for i in range(len(F)):
+        print(i, F[i])
+    print('\n')
+
+
+# imprime as arestas do grafo
+def printEdges(weights):
+    print('Arestas:')
+    for k, v in weights.items():
+        a, b = k
+        print('{} -> {} ^ {}'.format(a, b, v))
+    print('\n')
+
+
+# imprime o layout dos fragmentos
+def printFragmentLayout(F, order, positions, super_len):
+    for i in order:
+        cnt = 0
+        s = ''
+        while cnt != super_len:
+            if cnt == positions[i]:
+                s += F[i]
+                cnt += len(F[i])
+            else:
+                s += '-'
+                cnt += 1
+        print(s, i)
+
+
+# Exemplo de chamada do código:
+# python ep3.py 3 AGTATTGGCAATC AATCGATG ATGCAAACCT CCTTTTGG TTGGCAATCACT
 def main():
     t = int(sys.argv[1])
     F = sys.argv[2:]
@@ -60,8 +121,26 @@ def main():
     V = len(F)
 
     graph, weights, overlaps = buildGraph(F, t)
-    print(overlaps)
 
-    topologialSort(graph, V)
+    order = topologialSort(graph, V)
+
+    printVertex(F)
+    printEdges(weights)
+
+    superseq = F[order[0]]
+    init_position = []
+    init_position.insert(order[0], 0)
+    pos = 0
+    for s, t in zip(order, order[1:]):
+        w = weights[s, t]
+        superseq += F[t][w:]
+
+        pos += len(F[s]) - w
+        init_position.insert(t, pos)
+
+    print('Supersequência comum e Layout dos fragmentos:')
+    print(superseq)
+    printFragmentLayout(F, order, init_position, len(superseq))
+    print(superseq)
 
 main()
